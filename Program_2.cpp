709b5bc1ba9a630dbe539a46473a4adaffdc1256
João Miguel Martins <up201707311@fe.up.pt>
2018-03-26 17:11:10 +0100

    Realizado: - função para estrutar os dados do ficheiro (em vetor) - função de teste

---
+++ Program_2.cpp
@@ -0,0 +1,44 @@
+//
+// Created by joaom on 23-03-2018.
+//
+#include "Program_2.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Abre ficheiro e percorre cada linha extraindo para o vetor atraves do push_back
+void OpenToVec(ifstream &file_words, vector <string> &fileVec)
+{
+    file_words.open("Ex_P2.txt");
+    if (file_words.fail()) {
+        cerr << "Input file opening failed.\n";   // mensagem de erro ao abrir ficheiro
+        exit(1);
+    }
+    string line;
+    while (getline(file_words, line)) {
+        fileVec.push_back(line);
+    }
+    file_words.close();
+}
+
+//funcao de teste para verificar que o vetor tem todas as palavras do ficheiro
+void testingOTV (vector <string> fileVec)
+{
+    for (int i = 0; i < fileVec.size(); i++)
+    {
+        cout << fileVec[i]<<endl;
+    }
+}
+
+int main()
+{
+    ifstream file_words;
+    vector <string> fileVec;
+    OpenToVec(file_words,fileVec);
+    testingOTV(fileVec);
+    return 0;
+
+}
---
+++ Program_2.h
@@ -0,0 +1,10 @@
+//
+// Created by joaom on 23-03-2018.
+//
+
+#ifndef CRL_PROGRAM_2_H
+#define CRL_PROGRAM_2_H
+void OpenToVec(ifstream &file_words, vector <string> &fileVec);
+void testingOTV (vector <string> fileVec);
+
+#endif //CRL_PROGRAM_2_H
