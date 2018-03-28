c32ac607b7f8dfca4067bc7495df3a823c691f3d
João Miguel Martins <up201707311@fe.up.pt>
2018-03-28 16:16:19 +0100

    Realizado: - função para receber n letras do utilizador e devolver a lista de palavras formadas por essas letras; - funcao para pesquisar palavras com as letras dadas e retornar vetor com as palavras validas; - funcao para delimitar as palavras a pesquisar para aquelas com o mesmo tamanho da desejada;

--- Program_2.cpp
+++ Program_2.cpp
@@ -9,11 +9,10 @@ using namespace std;
 void OpenToVec(ifstream &file_words, vector<string> &fileVec) {
     try {
         file_words.open("Ex_P2.txt");
-    } catch (const exception&) {
+    }
+    catch (const exception &) {
         cerr << "Input file opening failed.\n";   // error message if opening file fails
         exit(1);
-    }
-    if (file_words.fail()) {
     }
     string line;
     while (getline(file_words, line)) {
@@ -23,43 +22,103 @@ void OpenToVec(ifstream &file_words, vector<string> &fileVec) {
 }
 
 //Test function to verify that the created vector has all the words from the list.
-void testingOTV(vector<string> fileVec) {
+void testingOTV(const vector<string> &fileVec) {
     for (int i = 0; i < fileVec.size(); i++) {
         cout << fileVec[i] << endl;
     }
 }
 
 // Binary search algorithm to find the input string in the word list, saved in a vector.
-bool BiSearch(const vector<string> &fileVec, const string &target) {
+/*
+ bool BiSearch(const vector<string> &fileVec, const string &target) {
     int min = 0;
     int max = fileVec.size() - 1;
+    bool found =
     int avg;
-    bool found = false;
-    while (min <= max) {
-        avg = lround((max + min) / 2 );
-        if (target == fileVec[avg]) {
-            found = true;
-            break;
-        } else if (fileVec[avg] < target) {
-            min = avg++;
-        } else {
-            max = avg--;
-        }
-    }
+     while (min <= max) {
+         avg = lround((max + min) / 2 );
+         if (target == fileVec[avg]) {
+             found = true;
+             break;
+         } else if (fileVec[avg] < target) {
+             min = avg++;
+         } else {
+             max = avg--;
+         }
+     }
     return found;
 }
+*/
 
 // Asks the user for the input string, searches it and check if its valid or not.
-void IsWordInList(vector<string> fileVec) {
+void IsWordInList(const vector<string> &fileVec) {
     string target;
     cout << "What is the word you want to search? (ALL CAPS ONLY) " << endl;
     cin >> target;
     bool valOp;
-    valOp = BiSearch(fileVec, target);
-    if (valOp) {
+    valOp = binary_search(fileVec.begin(), fileVec.end(),target);    // Using the binary search algorithm from std library
+    if (valOp) {                                                      // to ease the program building
         cout << "The word is valid." << endl;
     } else {
         cout << "The word is invalid. " << endl;
+    }
+}
+
+//function that searches word list for words with size N and selects them into a new vector to make search quicker
+vector<string> WordsNLetters(const vector<string> &fileVec, int Nsize) {
+    vector<string> valWords;
+    for (int i = 0; i < fileVec.size(); i++) {
+        if (fileVec[i].length() == Nsize) {
+            valWords.push_back(fileVec[i]);
+        }
+    }
+    return valWords;
+}
+
+
+//function that searches thorugh all valid words checking if they have the same words as in the set of letters, if so, they are added to a vector that will contain them
+vector<string> SearchWords(const vector<string> &valWords, const vector<string> &setLetters) {
+    vector<string> Poswords;
+    for (int i = 0; i < valWords.size(); i++) {
+        bool flag = true;
+        for (int i2 = 0; i2 < setLetters.size(); i2++) {
+            if (valWords[i].find_first_of(setLetters[i2]) ==
+                string::npos) {     //this condition checks if every letter of the user given set belongs to a certain word
+                flag = false;                                                    //if not, the flag is put false and the cycle breaks, proceeding to the next word
+                break;
+            }
+        }
+        if (flag) {
+            Poswords.push_back(valWords[i]);
+        }
+
+    }
+    return Poswords;
+}
+
+// function that asks the user for a set of letters and stores them into a vector, proceeding to search for valid words that contain the same given letters.
+void GiveWords(const vector<string> &fileVec) {
+    vector<string> setLetters;
+    cout << "Which letters do want to use to form words? (CTRL+Z to stop)";
+    string chr;
+    while (!cin.eof()) {                                  //a cycle to carry in every valid char (as string) input by user
+        cin >> chr;                                        // and store it in a vector that holds the set of N letters
+        setLetters.push_back(chr);
+    }
+    int Nsize = setLetters.size();
+    vector<string> valWords = WordsNLetters(fileVec,
+                                            Nsize);    //reduces the number of words to search for, to those with the same length as the number of letters given
+    vector<string> PosWords = SearchWords(valWords,
+                                          setLetters); // calls the function that will return the vector of valid words
+    if (PosWords.empty()) {
+        cout << "There is no match for the given letters."
+             << endl;    // if no words are found with the given letters, a error message shall appear on the screen
+    } else {
+        cout << "The list of valid words is: "
+             << endl;                  // the list of valid words are written into the screen, already ordered
+        for (int i = 0; i < PosWords.size(); i++) {
+            cout << PosWords[i] << endl;
+        }
     }
 }
 
@@ -67,9 +126,9 @@ int main() {
     ifstream file_words;
     vector<string> fileVec;
     OpenToVec(file_words, fileVec);
-    /* testingOTV(fileVec);*/
-    IsWordInList(fileVec);
-
+    /* testingOTV(fileVec);
+    IsWordInList(fileVec);*/
+    GiveWords(fileVec);
     return 0;
-
 }
+
--- Program_2.h
+++ Program_2.h
@@ -11,11 +11,16 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <ctime>
+#include <numeric>
 using namespace std;
 
-void OpenToVec(ifstream &file_words, vector <string> &fileVec);
-void testingOTV (vector <string> fileVec);
-bool BiSearch(vector<string> fileVec, string target);
-void IsWordInList(vector<string> fileVec);
+void OpenToVec(ifstream &file_words, vector<string> &fileVec);
+void testingOTV(const vector<string> &fileVec);
+bool BiSearch(const vector<string> &fileVec, const string &target);
+void IsWordInList(const vector<string> &fileVec);
+vector<string> WordsNLetters(const vector<string> &fileVec, int Nsize);
+vector<string> SearchWords(const vector<string> &valWords, const vector<string> &setLetters);
+void GiveWords(const vector<string> &fileVec);
 
 #endif //CRL_PROGRAM_2_H
